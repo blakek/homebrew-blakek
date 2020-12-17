8eb9b56c01383ef40d9fe3ae351aa8c435735d6b
Blake Knight <oss.ideas@gmail.com>
2020-12-16 22:59:42 -0600

    :arrow_up: update pomodoro package

--- Formula/pomodoro.rb
+++ Formula/pomodoro.rb
@@ -1,13 +1,13 @@
 class Pomodoro < Formula
   desc "🍅 A simple terminal-based pomodoro timer"
   homepage "https://github.com/blakek/pomodoro"
-  url "https://github.com/blakek/pomodoro/archive/v1.0.0.tar.gz"
-  sha256 "b1593911b9f763ef84655fade0895f2d542b174af31e38de8bef211658d0b67d"
+  url "https://github.com/blakek/pomodoro/archive/v2.0.0.tar.gz"
+  sha256 "e9443f583f62dd58653b321676aa0735a9e9973e1606a0c42adcdae575eda404"
 
-  depends_on "vlang" => :build
+  depends_on "go" => :build
 
   def install
-    system "make"
+    system "go build"
     bin.install "pomodoro"
   end
 
